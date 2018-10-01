057e7b604d54e0165c1df87fdd6406ad3c51f8d8
Erick Dave B. Dela Cruz <erickdavedelacruz@gmail.com>
2018-10-01 20:47:43 +0800

    Updates
    SERVER
    >> fix CheckExisting Notif bugs

--- Cordova/CheckExistingNotif.php
+++ Cordova/CheckExistingNotif.php
@@ -1,7 +1,7 @@
-<?php  
+<?php
 	require_once 'Config.php';
-	$query = 'SELECT * 
-			  FROM t_notification 
+	$query = 'SELECT *
+			  FROM t_notification
 			  WHERE type="newapp" and (status = "ignored" or status = "allowed" or status="unresolved") and appliance_id = "' . $appl_uid.'"';
 
 	$result = $con->query($query);
@@ -9,28 +9,29 @@
 	//--------------------------------------- CHECK IF EXISTING  ----------------------------
 	if(mysqli_num_rows($result) > 0){
 		$whilePluggedIn = true;
-		$statusQuery = 'SELECT status 
-						FROM t_notification 
+		$statusQuery = 'SELECT status
+						FROM t_notification
 						WHERE type="newapp" (status = "ignored" or status = "allowed") and appliance_id = "' . $appl_uid.'"';
 		$statusResult = $con->query($query);
 		if(mysqli_num_rows($statusResult) > 0 ){
-			$updateQuery = 'UPDATE t_notification 
-							SET status = "unresolved" 
-							WHERE type="newapp" and (status = "ignored" or status = "allowed") and appliance_id = "' . $appl_uid.'"
-							LIMIT 1,1';
+			$updateQuery = "UPDATE t_notification ".
+											"SET status = 'unresolved' ".
+											"WHERE notif_id = (SELECT MAX(tn.notif_id) from (select * from t_notification) as tn WHERE tn.type='newapp' and (tn.status = 'ignored' or tn.status = 'allowed') and tn.appliance_id = '" . $appl_uid . "') and type='newapp' and (status = 'ignored' or status = 'allowed') and appliance_id = '" . $appl_uid. "'";
+
+			echo "query: " . $updateQuery;
 			if($con->query($updateQuery)){
 				echo mysqli_error($con);
 			}
 		}
-	} 
+	}
 	else{
 		$query = 'INSERT INTO t_notification values ("","newapp","unresolved","'.$appl_uid.'")';
 		if($con->query($query)){
 			echo mysqli_error($con);
 		}
 	}
-		
-	
+
+
 	$appliance_arr=array(
 		"uid" => $appl_uid,
 		"has_power" => "0",//value change depeding on the value of the user select 0/1
@@ -40,8 +41,8 @@
 	// get from signedPowerData.php since this is impoted
 	//echo $UID."||". $voltage."||".$ampere."||". $power."||".$watthr."||".$date."||".$time."||".$timezone."\n\r";
 	echo  $json_has_power_data ;
-	
+
 	//create json file
 	file_put_contents('json_has_power_data.json',  $json_has_power_data);
-	
+
 ?>
