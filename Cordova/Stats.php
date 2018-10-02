8dfd7a5eee39fca53288ecd4af56995441e435c7
Erick Dave B. Dela Cruz <erickdavedelacruz@gmail.com>
2018-10-02 20:22:17 +0800

    Added How To
    intruction how to use tips the system

--- Cordova/Stats.php
+++ Cordova/Stats.php
@@ -1,29 +1,127 @@
 <?php
-  require 'Config.php';
+  require_once 'Config.php';
 
-  if(isset($_GET['getallstats'])){
-    $data = $_GET['getallstats'];
-    $arr = array();
-    $query = "SELECT uid from t_appliance";
-    $curhour = date('H');
-    $id = '6f63b28';
+  if(isset($_POST['getdaily'])){
+    $consumptionarray = array();
+    $hoursarray = array();
+    $id = $_POST['uid'];
+    $date = ($_POST['getdaily']=="now")? "NOW()" : "'".$_POST['getdaily']."'";
 
-    for ($hour=0; $hour <= $curhour; $hour++) {
-      if($data == "now"){
-        $select = "IFNULL(MAX(consumed), (SELECT IFNULL(MAX(th.consumed),0) from t_history as th where uid='$id' and  date(th.effective_date) = date(NOW()) and HOUR(th.effective_date) < $hour)) as  consumed";
-        $table = "t_history";
-        $where = "uid='$id' and  DATE(effective_date) and HOUR(effective_date) = ".$hour;
-        $result = processQuery($select,$table,$where);
+    //MinHours
+    $select = "IFNULL(MIN(HOUR(effective_date)),(HOUR(NOW()))-8) as MinHours , IFNULL(MAX(HOUR(effective_date)),HOUR(NOW())) as MaxHours";
+    $table = "t_history";
+    $where = "uid='".$id."' and DATE(effective_date) = DATE(".$date.")";
+    $result = processQuery($select,$table,$where);
 
-        if(mysqli_num_rows($result)>0){
-          while($row = mysqli_fetch_assoc($result)){
-            echo $row["consumed"]; echo " ";
-          }
-        } else {
-          print mysqli_error($con);
+    if(mysqli_num_rows($result)>0){
+      while($row = mysqli_fetch_assoc($result)){
+        $MinTime = intval($row['MinHours']);
+        $MaxTime = intval($row['MaxHours']);
+      }
+    } else {
+      print mysqli_error($con);
+    }
+
+    for ($i=$MinTime; $i <=$MaxTime; $i++) {
+      array_push($hoursarray,$i.":00");
+    }
+
+    //Retrieve Consumption
+    for ($i=$MinTime; $i <=$MaxTime; $i++) {
+      $select = "IFNULL(MAX(consumed), (SELECT IFNULL(MAX(th.consumed),0) from t_history as th where uid='$id' and  date(th.effective_date) = date(".$date.") and HOUR(th.effective_date) < $i)) as  consumed";
+      $table = "t_history";
+      $where = "uid='$id' and  DATE(effective_date)=DATE(".$date.") and HOUR(effective_date) = ".$i;
+      $result = processQuery($select,$table,$where);
+
+      if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_assoc($result)){
+          array_push($consumptionarray,intval($row["consumed"]));
         }
+      } else {
+        print mysqli_error($con);
       }
     }
+    echo json_encode($hoursarray)."|".json_encode($consumptionarray);
+  }
+
+  if(isset($_POST['getweekly'])){
+    $consumptionarray = array();
+    $daysarray = array();
+    $daysarray = array("SUN","MON","TUE","WED","THU","FRI","SAT");
+    $id = $_POST['uid'];
+    $day = intval($_POST['Day']);
+    $getdate = ($_POST['getweekly']=="now")? date("Y-m-d") : $_POST['getweekly'];
+    //get how many days from sunday and then subtract to getdate to get sunday's date
+    $newday = $day - 1;
+    $date = date('Y-m-d', strtotime($getdate. ' - ' . $newday . ' days'));
+    for ($i=0; $i < 7; $i++) {
+      $curdate = date('Y-m-d', strtotime($date. ' + ' . $i . ' days'));
+      $select = "IFNULL(MAX(consumed), (SELECT IFNULL(MAX(th.consumed),0) from t_history as th where uid='$id' and  YEAR(th.effective_date) = YEAR('".$curdate."') and MONTH(th.effective_date) = MONTH('".$curdate."') and DAY(th.effective_date) < DAY('".$curdate."'))) as  consumed";
+      $table = "t_history";
+      $where = "uid='$id' and  DATE(effective_date) = DATE('".$curdate."')";
+      $result = processQuery($select,$table,$where);
+
+      if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_assoc($result)){
+          array_push($consumptionarray,intval($row["consumed"]));
+        }
+      } else {
+        print mysqli_error($con);
+      }
+    }
+    echo json_encode($daysarray)."|".json_encode($consumptionarray);
+  }
+
+  if(isset($_POST['getmonthly'])){
+    $consumptionarray = array();
+    $monthsarray = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+    $id = $_POST['uid'];
+    $date = ($_POST['getmonthly']=="now")? date("Y-m-d") : $_POST['getmonthly'];
+
+    for ($i=1; $i <= 12; $i++) {
+      $select = "IFNULL(MAX(consumed), 0) as  consumed";
+      $table = "t_history";
+      $where = "uid='$id' and  YEAR(effective_date) = YEAR('".$date."') and MONTH(effective_date) = " . $i;
+      $result = processQuery($select,$table,$where);
+
+      if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_assoc($result)){
+          array_push($consumptionarray,intval($row["consumed"]));
+        }
+      } else {
+        print mysqli_error($con);
+      }
+    }
+    echo json_encode($monthsarray)."|".json_encode($consumptionarray);
+  }
+
+  if(isset($_POST['getyearly'])){
+    $consumptionarray = array();
+    $yearsarray = array();
+    $id = $_POST['uid'];
+    $date = ($_POST['getyearly']=="now")? date("Y-m-d") : $_POST['getyearly'];
+    $thedate = explode('-',$date);
+    $maxdate = explode('-',date("Y-m-d"));
+    if($thedate[0]==$maxdate[0]){
+      $thedate[0]=$maxdate[0] - 3;
+    }
+
+    for ($i=$thedate[0]; $i <=$maxdate[0]; $i++) {
+      array_push($yearsarray,$i);
+      $select = "IFNULL(MAX(consumed), 0) as  consumed";
+      $table = "t_history";
+      $where = "uid='$id' and  YEAR(effective_date) = " . $i;
+      $result = processQuery($select,$table,$where);
+
+      if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_assoc($result)){
+          array_push($consumptionarray,intval($row["consumed"]));
+        }
+      } else {
+        print mysqli_error($con);
+      }
+    }
+    echo json_encode($yearsarray)."|".json_encode($consumptionarray);
   }
 
  ?>
---
+++ Cordova/js/stats.js
@@ -0,0 +1,69 @@
+var selecteddate = "now";
+var uid = "6f63b28";
+var options  = ["getdaily" , "getweekly" , "getmonthly" , "getyearly"];
+var d = new Date();
+var dayOfWeek = d.getDay()+1;
+
+$(function() {
+  var instance = M.Datepicker.getInstance(document.getElementById("DateReference"));
+  instance.options.autoClose= true;
+
+  $('#DateReference').change(function(){
+    var dateObj = new Date($(this).val());
+    var month = dateObj.getMonth() + 1; //months from 1-12
+    var day = dateObj.getDate();
+    var year = dateObj.getFullYear();
+
+    var newdate = year + "-" + month + "-" + day;
+    selecteddate = newdate;
+    dayOfWeek = dateObj.getDay() + 1;
+    reloadCharts();
+  });
+
+  function getchartdata(type){
+    $.ajax({
+      type: "POST",
+      data: type + "=" + selecteddate + "&ts=" + $.now() + "&uid=" + uid + "&Day=" + dayOfWeek,
+      url: 'http://' + deviceHost + '/stats.php',
+      crossDomain: true,
+      contentType: "application/x-www-form-urlencoded; charset=utf-8",
+      success: function (data) {
+        var result = data.trim().split("|");
+        var labelset = JSON.parse(result[0].trim());
+        var chartset = JSON.parse(result[1].trim());
+        if(type==options[0]){
+          dailychart.config.data.datasets[0].data = chartset;
+          dailychart.config.data.labels = labelset;
+  				dailychart.update();
+        } else if(type==options[1]){
+          weekchart.config.data.datasets[0].data = chartset;
+          weekchart.config.data.labels = labelset;
+  				weekchart.update();
+        } else if(type==options[2]){
+          monthchart.config.data.datasets[0].data = chartset;
+          monthchart.config.data.labels = labelset;
+  				monthchart.update();
+          console.log(monthchart);
+        } else if(type==options[3]){
+          yearchart.config.data.datasets[0].data = chartset;
+          yearchart.config.data.labels = labelset;
+  			  yearchart.update();
+        }
+      }
+    });
+  }
+
+  function reloadCharts(){
+    for (var i = 0; i < 4; i++) {
+      getchartdata(options[i]);
+    }
+  }
+
+  $('#DateReference').change(function(){
+    var instance = M.Datepicker.getInstance($(this));
+  });
+
+  reloadCharts();
+
+
+});
