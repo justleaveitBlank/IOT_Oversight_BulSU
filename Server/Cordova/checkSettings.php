8e6fbdc7c0c25b03b79d53ec317811a92a5e1b7f
Erick Dave B. Dela Cruz <erickdavedelacruz@gmail.com>
2018-11-18 13:18:15 +0800

    UPDATE
    Check Settings Updated
    (note need alert to the front end to notify device authentication settings behavior)

--- Server/Cordova/checkSettings.php
+++ Server/Cordova/checkSettings.php
@@ -15,7 +15,61 @@
 	if($s_socket == "true"){
 		//------------------------------------- AUTHENTICATION -------------------------------------//
 		if($s_authentication == "true"){
-			//------------------------------------- LIMITATION HERE -------------------------------------//
+			$appQuery ='SELECT *
+					FROM t_appliance
+					WHERE uid="' .$UID. '"';
+			$appResult = $con->query($appQuery);
+			if($appResult->num_rows > 0){
+				echo "\r\n REGISTERED \r\n";
+				if($s_limitation == "true"){
+					//Check consumption vs limit take id if appliance exceed warning level
+					$query = "SELECT IF(current_power_usage/1000 < (power_limit_value*0.85),'NORMAL', IF(current_power_usage >= power_limit_value,'STOP','OVERCONSUMING')) as ConsumptionStatus FROM t_appliance WHERE power_limit_value>0 and uid = '".$UID."'";
+					$result = $con->query($query);
+					if(mysqli_num_rows($result)==1){
+						while ($row = mysqli_fetch_assoc($result)) {
+							$consumer_status = $row['ConsumptionStatus'];
+							if($consumer_status == "NORMAL"){
+								$query = "UPDATE t_notification set status = 'ignored' WHERE type = 'consumption'";
+								$con->query($query);
+								$has_power = 1;
+							} else {
+								$query = "SELECT IFNULL(MAX(notif_id),'NULL') as notif_id FROM t_notification WHERE type = 'consumption' and appliance_id = '".$UID."'";
+								$notif_results = $con->query($query);
+								if(mysqli_num_rows($notif_results)>0){
+									while ($notif_row = mysqli_fetch_assoc($notif_results)) {
+										$target_notif = $notif_row['notif_id'];
+										$query = "";
+										if($target_notif != "NULL"){
+											$query = "UPDATE t_notification SET status = 'unresolved' WHERE notif_id = ".$target_notif;
+										} else {
+											$query = "INSERT INTO t_notification (`type`, `status`, `appliance_id`) VALUES ('consumption','unresolved','".$UID."')";
+										}
+										$con->query($query);
+									}
+								}
+								if($consumer_status == "STOP"){
+									$query = "UPDATE t_appliance SET has_power = 0 WHERE uid = '".$UID."'";
+									if($con->query($query)){
+										echo mysqli_error($con);
+										$has_power = 0;
+									}
+								}
+							}
+						}
+					}
+				}
+				include_once 'generate_json_has_power.php';
+				echo "\r\n";
+			}
+			else{
+				echo "\r\n UNREGISTERED \r\n";
+				//output that is something on front end that is something is plugged 
+				//authentication is on unregistered and anonymous appliance access grant denied or something 
+				//please turn of  autentication on setting or rather register this appliance
+			}
+			
+		}
+		else{
 			if($s_limitation == "true"){
 				//Check whether temporary appliance have an id or not
 				$nameToCompare = ($UID == "NO_UID")? "Anonymous_Appliance" : "Unregistered_Appliance";
@@ -101,41 +155,6 @@
 					}
 				}
 			}
-
-			include_once 'generate_json_has_power.php';
-			echo "\r\n";
-		}
-		else{
-			$appQuery ='SELECT *
-					FROM t_appliance
-					WHERE uid="' .$UID. '"';
-
-			$appResult = $con->query($appQuery);
-
-			if(mysqli_num_rows($appResult)<1){
-				if($UID == "NO_UID"){
-					$query = "INSERT INTO t_appliance VALUES('".$UID."','Anonymous_Appliance',1,0,0,DEFAULT,DEFAULT,DEFAULT,DEFAULT,0,0,0,DEFAULT)";
-				} else {
-					$query = "INSERT INTO t_appliance VALUES('".$UID."','Unregistered_Appliance',1,0,0,DEFAULT,DEFAULT,DEFAULT,DEFAULT,0,0,0,DEFAULT)";
-				}
-
-				if($con->query($query)){
-					echo mysqli_error($con);
-				}
-			}
-			$notifStat = "true";
-			$has_power="1";
-			$appliance_arr=array(
-				"has_power" => $has_power,//value change depeding on the value of the user select 0/1
-				"socket_status" => "Socket On",
-				"authentication" => $s_authentication
-				);
-			//include_once 'insertToHistoryDB.php';
-			$json_has_power_data = json_encode($appliance_arr, JSON_PRETTY_PRINT);
-			//echo  $json_has_power_data ;
-
-			//create json file
-			//file_put_contents('json_has_power_data.json',  $json_has_power_data);
 
 			include_once 'generate_json_has_power.php';
 			echo "\r\n";
